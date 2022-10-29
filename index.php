83aee2bc7b6de642c3311400f76fbe8ae0d87ada
Bamidele Adedotun O <adedotunolusegun@yahoo.com>
2022-10-29 21:52:34 +0100

    content type added

--- index.php
+++ index.php
@@ -2,4 +2,9 @@
 
 $response = ["slackUsername" => "arakunrinbambam", "backend"=> true, "age" => 30, "bio" => "I'm a tech enthusiast, I love programming and I do music at my leisure"];
 
+header("Content-Type: application/json");
 echo json_encode($response);
+exit();
+
+
+?>
