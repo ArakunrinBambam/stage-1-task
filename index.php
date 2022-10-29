7e8a1e5e125962df405b42636ba7c4b6c5505333
Bamidele Adedotun O <adedotunolusegun@yahoo.com>
2022-10-29 21:11:25 +0100

    first commit for the task

---
+++ index.php
@@ -0,0 +1,5 @@
+<?php 
+
+$response = ["slackUsername" => "arakunrinbambam", "backend"=> true, "age" => 30, "bio" => "I'm a tech enthusiast, I love programming and I do music at my leisure"];
+
+echo json_encode($response);
